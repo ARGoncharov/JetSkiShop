55fb0288f1feb043fc1e9520ba192db0a5903b33
ARGoncharov <gonlex@mail.ru>
2023-06-07 15:05:26 +0300

    Adding slider to banner make an animation

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(function (){
-    $('.banner-section_slider').slick({});
+    $('.banner-section_slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section_slider-btn banner-section_slider-btnprev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="banner-section_slider-btn banner-section_slider-btnnext"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    });
+
 });
